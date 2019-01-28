42637c7f86e6bd719e63e6aa837cb6b9498cf275
אושרי ברוך <osher.baruch1@gmail.com>
2019-01-28 15:39:44 +0200

    space

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 
+
 app.listen(process.env.PORT || 1996, function() {
   console.log("Server up and running on port 1996")
 })
