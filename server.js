e4c2cf50fb5c33dcfa614f05dc7a0f5c016fa1bd
אושרי ברוך <osher.baruch1@gmail.com>
2019-01-28 15:43:05 +0200

    dynos

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 
-
 app.listen(process.env.PORT || 1996, function() {
   console.log("Server up and running on port 1996")
 })
