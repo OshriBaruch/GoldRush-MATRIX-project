ed4f0a1720a7c4c180e51f8bbc063ae8759565b3
אושרי ברוך <osher.baruch1@gmail.com>
2019-01-28 11:41:01 +0200

    another fixed

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 const express = require('express')
-const api = require('./server/routes/api')
-const bodyParser = require('body-parser')
 const path = require('path')
-
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/weatherDB")
 
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
-app.use('/', api)
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
 
 app.listen(process.env.PORT || 1996, function() {
   console.log("Server up and running on port 1996")
